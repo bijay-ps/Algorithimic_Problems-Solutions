b28d9ca101052470907683c02afef798b14850fe
bijay_ps <bijay.prakash7@yahoo.in>
2020-07-17 13:01:06 +0530

    cosmetic changes

--- batman_bijay/repeatedString.py
+++ batman_bijay/repeatedString.py
@@ -10,13 +10,13 @@ def countAInRepeatedString(s, n):
     return repeated_count
 
 
-print(countAInRepeatedString('aba', 10))  # aba-aba-aba-a
+print(countAInRepeatedString('aba', 10))
 print(countAInRepeatedString('abcda', 257))
 print(countAInRepeatedString('abc', 343))
 print(countAInRepeatedString('abc', 50000))
 print(countAInRepeatedString('a', 1000000000000))
 print(countAInRepeatedString('abaca', 1000000000000000))
 print(countAInRepeatedString('a', 1000000000000000))
-print(countAInRepeatedString('abcac', 10))  # abcac-abcac
-print(countAInRepeatedString('aaa', 10))  # aaa-aaa-aaa-a
-print(countAInRepeatedString('aa', 10))  # aa-aa-aa-aa-aa
+print(countAInRepeatedString('abcac', 10))
+print(countAInRepeatedString('aaa', 10))
+print(countAInRepeatedString('aa', 10))
