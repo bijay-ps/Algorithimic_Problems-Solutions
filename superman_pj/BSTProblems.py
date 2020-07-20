ccc2cc26592bf113d081687711e37e3ecd779bcb
PJ-home <being_yuva@hotmail.com>
2020-07-20 23:23:38 +0530

    modified: BST problems

--- superman_pj/BSTProblems.py
+++ superman_pj/BSTProblems.py
@@ -1,4 +1,9 @@
 # Binary Search Tree implementation with basic operations
+import collections
+
+nodes = []
+
+
 class BST:
     root = None
 
@@ -100,6 +105,70 @@ class BST:
                 return True
         return False
 
+    # https://leetcode.com/problems/same-tree/
+    def isSameTree(self, p: Node, q: Node) -> bool:
+        if p == None and q == None:
+            return True
+        if p != None and q != None:
+            return p.value == q. value and self.isSameTree(p.left_child, q.left_childft) and self.isSameTree(p.right_childht, q.right_childht)
+        return False
+
+    # https://leetcode.com/problems/validate-binary-search-tree
+    # use recursion to compare each node is within the range of accepted values
+    def isValidBST(self, root) -> bool:
+        return self.isValidNode(root, float('-inf'), float('inf'))
+
+    def isValidNode(self, root, min, max):
+        if root == None:
+            return True
+        if root.val < min or root.val > max:
+            return False
+        return self.isValidNode(root.left, min, root.val - 1) and self.isValidNode(root.right, root.val + 1, max)
+
+    # https://leetcode.com/problems/second-minimum-node-in-a-binary-tree
+
+    def findSecondMinimumValue(self, root):
+        self.ans = float('inf')
+        min1 = root.value
+
+        def dfs(node):
+            if node:
+                if min1 < node.value < self.ans:
+                    self.ans = node.value
+                elif node.value == min1:
+                    dfs(node.left_child)
+                    dfs(node.right_child)
+
+        dfs(root)
+        return self.ans if self.ans < float('inf') else -1
+
+
+def heightofTree(root) -> int:
+    if root == None:
+        return -1
+    if root.left_child == None and root.right_child == None:
+        return 0
+
+    return 1 + max(heightofTree(root.left_child), heightofTree(root.right_child))
+
+
+def getNodesatDistance(root, distance):
+    if root != None:
+        if distance == 0:
+            nodes.append(root.val)
+            return
+        getNodesatDistance(root.left, distance - 1)
+        getNodesatDistance(root.right, distance - 1)
+
+
+def levelOrderTraversal(root):
+    temp = []
+    for i in range(1, heightofTree(root) + 1):
+        getNodesatDistance(root, i)
+        for val in nodes:
+            temp.append(val)
+    return temp
+
 
 def treeNodeToString(root):
     if not root:
@@ -124,11 +193,8 @@ def treeNodeToString(root):
 if __name__ == "__main__":
     tree = BST()
     tree.insert(2)
-    tree.insert(0)
     tree.insert(3)
     tree.insert(2)
     tree.insert(4)
     tree.insert(7)
-    print(treeNodeToString(tree.root))
-    print(tree.remove(3))
-    print(treeNodeToString(tree.root))
+    print(heightofTree(tree.root))
