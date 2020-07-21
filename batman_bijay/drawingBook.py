f99c25c5e8906958472fdff2de16d5e23a61cca7
bijay_ps <bijay.prakash7@yahoo.in>
2020-07-21 10:22:36 +0530

    drawingBook solution

---
+++ batman_bijay/drawingBook.py
@@ -0,0 +1,19 @@
+# https://www.hackerrank.com/challenges/drawing-book/problem
+
+def pageCount(n, p):
+    return min(p//2, n//2-p//2)
+
+
+res1 = pageCount(6, 2)  # 1
+res2 = pageCount(5, 4)  # 0
+res3 = pageCount(7, 3)  # 1
+res4 = pageCount(6, 4)  # 1
+res5 = pageCount(53, 26)  # 13
+res6 = pageCount(6, 5)  # 1
+
+print(res1)
+print(res2)
+print(res3)
+print(res4)
+print(res5)
+print(res6)
