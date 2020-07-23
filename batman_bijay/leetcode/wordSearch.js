cf339cee9bb8badf86edbcaea364209c18141483
bijay_ps <bijay.prakash7@yahoo.in>
2020-07-23 14:33:54 +0530

    finally!!! I solved it. Took me almost 5 hours though 🤯🤯 I need tonnes of more practise

--- batman_bijay/leetcode/wordSearch.js
+++ batman_bijay/leetcode/wordSearch.js
@@ -8,43 +8,63 @@ Y [
  2 --  ['A','D','E','E']
    X --  0   1   2   3
 ]
+
+word = SEE
 */
 
 function wordSearch(board, word) {
-  const given_word = word;
-  const indices = findIndices(given_word[0], board);
-  // console.log("indices: ", indices);
-  findAdjacent(word[0], indices, board);
-}
+  // 'count' is how many matching letters we have found
+  // i is row
+  // j is column
+  let matchAllRemainingLetters = (board, i, j, word, count = 0) => {
+    if (count === word.length) return true;
+    if (
+      i < 0 ||
+      i >= board.length ||
+      j < 0 ||
+      j >= board[0].length ||
+      board[i][j] !== word[count]
+    )
+      return false;
+    let temp = board[i][j];
+    board[i][j] = "";
+    let found =
+      matchAllRemainingLetters(board, i - 1, j, word, count + 1) ||
+      matchAllRemainingLetters(board, i + 1, j, word, count + 1) ||
+      matchAllRemainingLetters(board, i, j - 1, word, count + 1) ||
+      matchAllRemainingLetters(board, i, j + 1, word, count + 1);
+    board[i][j] = temp;
+    return found;
+  };
 
-function findIndices(char, board) {
-  let indices = [];
-  board.forEach((subArr, index) => {
-    subArr.forEach((e, i) => {
-      if (e === char) {
-        let temp = { [index]: i };
-        indices.push(temp);
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (
+        board[i][j] === word[0] &&
+        matchAllRemainingLetters(board, i, j, word)
+      ) {
+        return true;
       }
-    });
-  });
-  return indices;
-}
-
-function findAdjacent(X, xIndices, board) {
-  let adjLetters = [];
-  console.log("line 35", xIndices);
-  const Yaxis = Object.keys(xIndices);
-  console.log("line 37", Yaxis);
-  Yaxis.forEach((e) => {});
+    }
+  }
+  return false;
 }
 
 const board = [
   ["A", "B", "C", "E"],
-
   ["S", "F", "C", "S"],
-
   ["A", "D", "E", "E"],
 ];
 
-wordSearch(board, "S");
-// wordSearch(board, "E");
+const res1 = wordSearch(board, "SEE");
+const res2 = wordSearch(board, "SEA");
+const res3 = wordSearch(board, "S");
+const res4 = wordSearch(board, "C");
+const res5 = wordSearch(board, "ABCCED");
+const res6 = wordSearch(board, "ABCB");
+console.log(res1);
+console.log(res2);
+console.log(res3);
+console.log(res4);
+console.log(res5);
+console.log(res6);
